b06917fcffebf951a4f4576cc809c9951490c33f
Lucas Vargas <lucas.vargas@alayacare.com>
2021-04-17 23:32:29 -0400

    Added a route for accounts/transactions and added a middleware for handling CORS

--- app/app.js
+++ app/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3030;
+const accounts = require('./routes/accounts');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+const corsOptions = {
+  origin: 'http://localhost',
+};
+
+app.use(cors(corsOptions));
+
+app.use('/accounts', accounts);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
