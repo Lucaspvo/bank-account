bde0ee69bef6092af5043089a99ed72f2cc85596
Lucas Vargas <lucas.vargas@alayacare.com>
2021-04-17 23:21:33 -0400

    Fixed code style

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Switch, Route, Redirect} from "react-router-dom";
 import TopNavBar from './components/nav-bars/top-nav-bar.js';
 import SideNavBar from './components/nav-bars/side-nav-bar.js';
@@ -6,20 +7,20 @@ import TransactionsList from './components/accounts/transactions/transactions-li
 
 function App() {
   return (
-  <div>
-    <TopNavBar/>
-    <div className="side-bar-and-content">
-      <SideNavBar/>
-      <Switch>
-        <Route path="/accounts/transactions">
-          <TransactionsList/>
-        </Route>
-        <Route path="/">
-          <Redirect to="/accounts/transactions"/>
-        </Route>
-      </Switch>
+    <div>
+      <TopNavBar/>
+      <div className="side-bar-and-content">
+        <SideNavBar/>
+        <Switch>
+          <Route path="/accounts/transactions">
+            <TransactionsList/>
+          </Route>
+          <Route path="/">
+            <Redirect to="/accounts/transactions"/>
+          </Route>
+        </Switch>
+      </div>
     </div>
-  </div>
   );
 }
 
