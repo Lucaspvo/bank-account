e2c6c9bf181dada76ce790c0ec200e3b61deb8cc
Lucas Vargas <lucas.vargas@alayacare.com>
2021-05-04 07:29:08 -0400

    Connected side nav bar to the store

--- web/src/components/nav-bars/side-nav-bar.js
+++ web/src/components/nav-bars/side-nav-bar.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import './side-nav-bar.css';
+import {connect} from 'react-redux';
+import {changeTabActive} from '../../actions/side-nav';
+
+const TABS = {
+  add: 'add-transaction',
+  list: 'transactions',
+};
+
+function mapStateToProps(state) {
+  return {
+    tabActive: state.sideNav.tabActive,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onTabChange(data) {
+      dispatch(changeTabActive(data));
+    },
+  };
+}
 
 class SideNavBar extends React.Component {
+  onTabChange(event, tabType) {
+    event.preventDefault();
+    this.props.onTabChange(tabType)
+  }
+
   render() {
     return (
       <div className="side-nav-bar-section">
         <div className="inner-side-nav-bar">
-          <div className="transactions-tab">
-            <label>Transactions</label>
-          </div>
+          <a onClick={event => this.onTabChange(event, TABS.add)} href="/accounts/transactions">
+            <div className={`add-transactions-tab ${this.props.tabActive === TABS.add ? 'active' : ''}`}>
+              <label>Add transactions</label>
+            </div>
+          </a>
+          <a onClick={event => this.onTabChange(event, TABS.list)} href="/accounts/transactions">
+            <div className={`transactions-tab ${this.props.tabActive === TABS.list ? 'active' : ''}`}>
+              <label>Transactions</label>
+            </div>
+          </a>
         </div>
       </div>
     );
   }
 }
 
-export default SideNavBar;
+export default connect(mapStateToProps, mapDispatchToProps)(SideNavBar);
