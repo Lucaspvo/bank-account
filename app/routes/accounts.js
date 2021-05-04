f594eadc59f0ef8cfc4d2bac88e83d06f54c0af7
Lucas Vargas <lucas.vargas@alayacare.com>
2021-05-04 07:27:03 -0400

    Added put and delete endpoints

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -1,7 +1,18 @@
-let {getCollectionTransactions} = require('../modules/accounts/transactions/resources.js');
+let {
+  getCollectionTransactions,
+  postCollectionTransactions,
+  putCollectionTransactions,
+  deleteCollectionTransactions
+} = require('../modules/accounts/transactions/resources.js');
 let express = require('express');
 let router = express.Router();
 
 router.get('/:accountId/transactions', getCollectionTransactions);
+
+router.post('/:accountId/transactions', postCollectionTransactions);
+
+router.put('/:accountId/transactions/:transactionId', putCollectionTransactions);
+
+router.delete('/:accountId/transactions/:transactionId', deleteCollectionTransactions);
 
 module.exports = router;
