90d7ef78f92c41836e292f17ac8614c58af85d77
Lucas Vargas <lucas.vargas@alayacare.com>
2021-05-04 07:28:06 -0400

    Added resources for put/delete as well as repositories to manage on the data side

--- app/app.js
+++ app/app.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const app = express();
 const port = 3030;
 const accounts = require('./routes/accounts');
+const bodyParser = require('body-parser')
 
 const corsOptions = {
   origin: 'http://localhost',
 };
 
 app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 app.use('/accounts', accounts);
 
--- app/modules/accounts/transactions/resources.js
+++ app/modules/accounts/transactions/resources.js
@@ -1,11 +1,37 @@
 const TransactionRepository = require('./repositories');
-const repository = new TransactionRepository('transactions');
+const TransactionAdapter = require('./adapter');
+const TransactionSchema = require('./schema');
+
+const adapter = new TransactionAdapter(TransactionRepository);
+const schema = new TransactionSchema();
 
 const getCollectionTransactions = function (req, res) {
-  const response = repository.filter(req.query);
+  const response = adapter.filter(req.query);
   res.send(response);
+};
+
+const postCollectionTransactions = function (req, res, next) {
+  const body = schema.validate(req, next);
+  const response = adapter.save(body);
+  res.status(201).send(response);
+};
+
+const putCollectionTransactions = function (req, res, next) {
+  const body = schema.validate(req, next);
+  const {transactionId} = req.params;
+  const response = adapter.update(transactionId, body);
+  res.send(response);
+};
+
+const deleteCollectionTransactions = function (req, res) {
+  const {transactionId} = req.params;
+  adapter.delete(transactionId);
+  res.status(204).send();
 };
 
 module.exports = {
   getCollectionTransactions,
+  postCollectionTransactions,
+  putCollectionTransactions,
+  deleteCollectionTransactions
 };
